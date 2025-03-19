5ae08ab57ed4703dfdc78919a24388f1b7ac9b57
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-03-19 15:11:59 +0800

    fix: add repository to generated bindings

--- gen.py
+++ gen.py
@@ -2,11 +2,128 @@ import json
 from pathlib import Path
 import subprocess
 import shutil
+import semver
+import os
+
+
+MOON_HOME = Path(os.getenv("MOON_HOME"))
+VERSION = "0.1.6"
+
+
+class Grammar:
+    name: str
+    path: Path
+    old_version: semver.VersionInfo
+    old_repository: str
+    repository: str
+    stubs: list[str]
+    files: list[str]
+
+    def __init__(self, name: str, path: Path, repository: str, commit: str):
+        self.name = name
+        self.path = path
+        self.repository = f"{repository}#{commit}"
+        self.stubs = []
+        self.files = []
+        src_path = path / "src"
+        for file in src_path.iterdir():
+            if file.suffix == ".c":
+                self.stubs.append(str(file.relative_to(src_path)))
+            self.files.append(str(file.relative_to(src_path)))
+
+    def generate_gitignore_to(self, destination: Path):
+        content = "\n".join(self.files) + "\n"
+        destination.write_text(content)
+
+    def generate_moon_mod_json_to(self, destination: Path, version: str):
+        moon_mod_json = {
+            "name": f"tonyfettes/tree_sitter_{self.name}",
+            "version": version,
+            "deps": {
+                "tonyfettes/tree_sitter_language": "0.1.1",
+            },
+            "repository": self.repository,
+            "license": "Apache-2.0",
+            "include": self.files + ["binding.mbt", "moon.pkg.json"],
+        }
+        destination.write_text(json.dumps(moon_mod_json, indent=2) + "\n")
+
+    def generate_moon_pkg_json_to(self, destination: Path):
+        moon_pkg_json = {
+            "import": ["tonyfettes/tree_sitter_language"],
+            "native_stub": self.stubs,
+            "support-targets": ["native"],
+        }
+        destination.write_text(json.dumps(moon_pkg_json, indent=2) + "\n")
+
+    def generate_binding_mbt_to(self, destination: Path):
+        content = f"""///|
+pub extern "c" fn language() -> @tree_sitter_language.Language = "tree_sitter_{self.name}"
+"""
+        destination.write_text(content)
+
+    def generate_binding_to(self, destination: Path):
+        def bump_version(version):
+            return semver.bump_patch(version)
+
+        version = VERSION
+        if destination.exists():
+            if (destination / "moon.mod.json").exists():
+                moon_mod_json = json.loads((destination / "moon.mod.json").read_text())
+                if "version" in moon_mod_json:
+                    version = moon_mod_json["version"]
+                    if semver.compare(version, VERSION) < 0:
+                        version = VERSION
+                    if "repository" in moon_mod_json:
+                        repository = moon_mod_json["repository"]
+                        if repository != self.repository:
+                            version = semver.bump_patch(version)
+                    else:
+                        version = semver.bump_patch(version)
+            shutil.rmtree(destination)
+        print(f"Generating binding for {self.name} at {destination}, version {version}")
+        shutil.copytree(self.path / "src", destination)
+        self.generate_gitignore_to(destination / ".gitignore")
+        self.generate_moon_mod_json_to(destination / "moon.mod.json", version)
+        self.generate_moon_pkg_json_to(destination / "moon.pkg.json")
+        self.generate_binding_mbt_to(destination / "binding.mbt")
+
+
+def git_submodule_url(path: Path) -> str:
+    try:
+        url = subprocess.run(
+            ["git", "config", f"submodule.{path}.url"],
+            capture_output=True,
+            check=True,
+            text=True,
+        )
+        url = url.stdout.strip()
+    except subprocess.CalledProcessError:
+        raise ValueError(f"Could not find submodule URL for {path}")
+    return url
+
+
+def git_submodule_commit(path: Path) -> str:
+    try:
+        commit = subprocess.run(
+            ["git", "submodule", "status", path],
+            capture_output=True,
+            check=True,
+            text=True,
+        )
+        commit = commit.stdout.strip().split(" ")[0]
+    except Exception as e:
+        raise ValueError(f"Could not get commit hash for {path}: {e}")
+
+    return commit
 
 
 def generate_binding(path: Path, bindings: Path):
     if not path.exists():
         raise FileNotFoundError(f"{path} does not exist")
+
+    url = git_submodule_url(path)
+    commit = git_submodule_commit(path)
 
     tree_sitter_json_path = path / "tree-sitter.json"
     if not tree_sitter_json_path.exists():
@@ -20,51 +137,10 @@ def generate_binding(path: Path, bindings: Path):
             grammar_path = grammar["path"]
         print(f"Generating binding for {grammar_name} at {grammar_path}")
         grammar_path: Path = path / grammar_path
-        c_sources: list[str] = []
-        all_sources: list[str] = []
-        for parser_file in (grammar_path / "src").iterdir():
-            if parser_file.suffix == ".c":
-                c_sources.append(str(parser_file.relative_to(grammar_path / "src")))
-            all_sources.append(str(parser_file.relative_to(grammar_path / "src")))
-
+        grammar = Grammar(grammar_name, grammar_path, url, commit)
         binding_root: Path = (bindings / f"tree_sitter_{grammar_name}").resolve()
-        print(f"Binding root: {binding_root}")
-        shutil.rmtree(binding_root, ignore_errors=True)
 
-        shutil.copytree(grammar_path / "src", binding_root)
-
-        (binding_root / ".gitignore").write_text("\n".join(all_sources) + "\n")
-
-        moon_mod_json = {
-            "name": f"tonyfettes/tree_sitter_{grammar_name}",
-            "version": "0.1.5",
-            "deps": {
-                "tonyfettes/tree_sitter_language": "0.1.1",
-            },
-            "license": "Apache-2.0",
-            "include": all_sources + [
-                "binding.mbt", "moon.pkg.json"
-            ],
-        }
-        (binding_root / "moon.mod.json").write_text(
-            json.dumps(moon_mod_json, indent=2) + "\n"
-        )
-
-        moon_pkg_json = {
-            "import": ["tonyfettes/tree_sitter_language"],
-            "native_stub": c_sources,
-            "support-targets": ["native"],
-        }
-        (binding_root / "moon.pkg.json").write_text(
-            json.dumps(moon_pkg_json, indent=2) + "\n"
-        )
-
-        binding_mbt = f"""///|
-pub extern "c" fn language() -> @tree_sitter_language.Language = "tree_sitter_{grammar_name}"
-"""
-        (binding_root / "binding.mbt").write_text(binding_mbt)
-
-        print(f"Building {grammar_name}")
+        grammar.generate_binding_to(binding_root)
 
         subprocess.run(
             ["moon", "build", "--target", "native"], cwd=binding_root, check=True
